4bb63feafbc5e7ff84d117229604388aa2165ed3
Adam Pawlak <flammy@pm.me>
2020-11-26 17:16:27 +0100

    Add project files.

---
+++ Adam.Pawlak.Service1.Tests/WeatherForecastController.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Adam.Pawlak.Service1.Tests
+{
+    public class WeatherForecastController
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Adam.Pawlak.Service2.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Adam.Pawlak.Service2.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
